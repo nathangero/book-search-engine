4a882b27b515a4f72b594f0e9e9dc67cd5c3385f
Nathan Geronimo <nathangero@yahoo.com>
2023-12-05 19:29:36 -0800

    add apollo and graphql dependencies and code

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,23 @@
+const typeDefs = `
+  type User {
+
+  }
+
+  type Book {
+
+  }
+
+  type Auth {
+    token: String
+  }
+
+  type Query {
+
+  }
+
+  type Mutation {
+
+  }
+`
+
+module.exports = typeDefs;
