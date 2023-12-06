ab48b37feea647143f9bb866d968a7155e009f99
Nathan Geronimo <nathangero@yahoo.com>
2023-12-05 20:33:55 -0800

    fill out typeDefs

---
+++ client/src/utils/queries.js
@@ -0,0 +1,5 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_SINGLE_USER = gql`
+    
+`;
--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,22 +1,37 @@
 const typeDefs = `
   type User {
-
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
   }
 
   type Book {
-
+    _id: ID
+    bookId: String
+    title: String
+    description: String
+    authors: [String]
+    image: String
+    link: String
   }
 
   type Auth {
-    token: String
+    token: ID!
+    user: User
   }
 
   type Query {
-
+    getSingleUser(userId: ID!): User
   }
 
   type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
 
+    saveBook(userId: ID!, book: Book!): User
+    delete(userId: ID!, bookId: ID): User
   }
 `
 
